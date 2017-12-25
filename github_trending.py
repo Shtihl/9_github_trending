f16346ed5dbb9f68868235d32f34fbc7023484f0
Shtihl <shtihlfd@yandex.ru>
2017-12-25 17:27:08 +0300

    some refactor

--- github_trending.py
+++ github_trending.py
@@ -12,40 +12,35 @@ def get_trending_repositories(quantity):
         'sort': 'stars',
         'order': 'desc'
     }
+    url = 'https://api.github.com/search/repositories'
     trending_repos = requests.get(
-        'https://api.github.com/search/repositories', params=search_params
-    ).json()['items'][:quantity]
+        url,
+        params=search_params
+    ).json()['items']
     return trending_repos
 
 
-def get_repositories_info(repositories):
-    repo_info = []
-    for repo in repositories:
-        repo_info.append({
-            'repo_owner' : repo['owner']['login'],
-            'repo_name' : repo['name'],
-            'repo_html_url' : repo['html_url'],
-            'repo_issues_quantity' : len(
-                requests.get('https://api.github.com/repos/{}/{}/issues'.format(
-                    repo['owner']['login'],
-                    repo['name']
-                )).json()
-            )
-        })
-    return repo_info
+def get_open_issues(repositories):
+    issues = []
+    for repository in repositories:
+        url = 'https://api.github.com/repos/{}/issues'.format(
+            repository['full_name']
+        )
+        issues.append(requests.get(url).json())
+    return issues
 
 
-def print_repositories_info(repositories):
-    for repo in repositories:
-        print('Repo Owner: \t\t{}'.format(repo['repo_owner']))
-        print('Repo Name: \t\t{}'.format(repo['repo_name']))
-        print('Open Issues Amount: \t{}'.format(repo['repo_issues_quantity']))
-        print('Repo Link: \t\t{}'.format(repo['repo_html_url']))
+def print_repositories_info(repositories, issues):
+    for repo, issue in zip(repositories, issues):
+        print('Repo Owner: \t\t{}'.format(repo['owner']['login']))
+        print('Repo Name: \t\t{}'.format(repo['name']))
+        print('Open Issues Amount: \t{}'.format(len(issue)))
+        print('Repo Link: \t\t{}'.format(repo['html_url']))
         print('-' * 80)
 
 
 if __name__ == '__main__':
     trending_repos_quantity = 20
     trending_repositories = get_trending_repositories(trending_repos_quantity)
-    trending_repositories_info = get_repositories_info(trending_repositories)
-    print_repositories_info(trending_repositories_info)
+    open_issues = get_open_issues(trending_repositories)
+    print_repositories_info(trending_repositories, open_issues)
